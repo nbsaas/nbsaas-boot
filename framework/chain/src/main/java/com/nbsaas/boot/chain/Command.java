ea610011ebe0f2ce5a3072ff33bc33489652fc5d
ada.young <735529126@qq.com>
2023-05-20 12:18:16 +0800

    增加command功能

--- framework/chain/src/main/java/com/nbsaas/boot/chain/Command.java
+++ framework/chain/src/main/java/com/nbsaas/boot/chain/Command.java
@@ -3,8 +3,35 @@ package com.nbsaas.boot.chain;
 import com.nbsaas.boot.rest.request.RequestObject;
 import com.nbsaas.boot.rest.response.ResponseObject;
 
-public interface Command<Request extends RequestObject, Response> {
+import java.util.Objects;
+
+public interface Command<Request extends RequestObject, Response>{
 
     ResponseObject<Response> execute(Request request);
+
+    default Command<Request, Response> before(Command<Request, Response> command) {
+        Objects.requireNonNull(command);
+        return (Request request) -> {
+            ResponseObject<Response> temp = command.execute(request);
+            if (temp.getCode() == 200){
+                return execute(request);
+            } else{
+                return temp;
+            }
+        };
+    }
+
+    default Command<Request, Response> after(Command<Request, Response> command) {
+        Objects.requireNonNull(command);
+
+        return (Request request) -> {
+            ResponseObject<Response> temp = execute(request);
+            if (temp.getCode() == 200){
+                return command.execute(request);
+            } else{
+                return temp;
+            }
+        };
+    }
 
 }
