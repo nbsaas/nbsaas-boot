40283f1defbf0bff583949161fa491953f798167
ada.young <735529126@qq.com>
2023-05-31 00:14:44 +0800

    增加版权

--- framework/jpa-dao/src/main/java/com/nbsaas/boot/jpa/data/entity/AbstractStateEntity.java
+++ framework/jpa-dao/src/main/java/com/nbsaas/boot/jpa/data/entity/AbstractStateEntity.java
@@ -17,25 +17,6 @@
  *
  */
 
-/*
- *
- *  * 版权声明和许可协议
- *  *
- *  * 版权所有 (c) 2023 纽百特®
- *  * 版权所有，保留所有权利
- *  *
- *  * 本软件使用 MIT 许可协议进行许可，详情请参阅：
- *  *
- *  *   https://opensource.org/licenses/MIT
- *  *
- *  * 更多信息，请访问我们的网站：
- *  *
- *  *   http://www.nbsaas.com
- *  *
- *  * 纽百特® 是西安纽百特科技有限责任公司的注册商标，未经授权不得使用。
- *
- */
-
 package com.nbsaas.boot.jpa.data.entity;
 
 import com.nbsaas.boot.rest.enums.StoreState;
