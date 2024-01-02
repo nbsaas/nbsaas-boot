13c568ee5d74e64d9d10820c568250b41760c018
ada.young <cng1985@gmail.com>
2024-01-02 22:48:56 +0800

    新增mybatis mapper文件夹生成

---
+++ code-generator/src/main/java/com/nbsaas/boot/generator/command/mybatis/MapperXmlCommand.java
@@ -0,0 +1,32 @@
+package com.nbsaas.boot.generator.command.mybatis;
+
+import com.nbsaas.boot.generator.command.common.BaseCommand;
+import com.nbsaas.boot.generator.config.Config;
+import com.nbsaas.boot.generator.context.InputRequestObject;
+import com.nbsaas.boot.rest.response.ResponseObject;
+
+import java.io.File;
+
+public class MapperXmlCommand extends BaseCommand {
+    @Override
+    public ResponseObject handle(InputRequestObject inputRequestObject) {
+        Config config = this.inputRequestObject.getConfig();
+        this.makeXmlPackage("." + config.getModuleName() + ".data.mapper");
+        return ResponseObject.success();
+
+    }
+    protected void makeXmlPackage(String basePackage) {
+        String packageTemp = this.inputRequestObject.getConfig().getBasePackage() + basePackage;
+        String requestDir = this.outPath() + "\\src\\main\\resources\\" + packageTemp.replace(".", "\\");
+        File out = new File(requestDir);
+        if (!out.exists()) {
+            out.mkdirs();
+        }
+
+    }
+    @Override
+    public String outPath() {
+        Config config = this.inputRequestObject.getConfig();
+        return config.getMultiple() ? config.getOutputPath() + "\\resources\\[[]]-resource".replace("[[]]", config.getProjectName()) : config.getOutputPath();
+    }
+}
