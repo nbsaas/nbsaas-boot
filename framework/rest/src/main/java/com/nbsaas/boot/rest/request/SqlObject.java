5855cf48d24cd868745896d3db802963b50ff95f
ada <735529126@qq.com>
2024-04-25 13:19:10 +0800

    完善sql对象

--- framework/rest/src/main/java/com/nbsaas/boot/rest/request/SqlObject.java
+++ framework/rest/src/main/java/com/nbsaas/boot/rest/request/SqlObject.java
@@ -6,10 +6,34 @@ import lombok.Data;
 @Data
 public class SqlObject {
 
+    /**
+     * sql 语句
+     */
     private String sql;
 
     private Long page;
 
     private Long size;
 
+    /**
+     * 1 分页 2 列表 3 统计 4单对象 5单值 6执行sql
+     */
+    private Integer queryType;
+
+    private Integer sortNum;
+
+    /**
+     * 1 设置变量 2 用于后续计算 3 无
+     */
+    private Integer resultType;
+
+    /**
+     * 变量名
+     */
+    private String varName;
+
+    /**
+     * 执行类型 1串行 2并行
+     */
+    private Integer execType;
 }
